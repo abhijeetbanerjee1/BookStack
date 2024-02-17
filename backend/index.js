819645fd98c765b1a22f71c61405bae558d5f244
Abhijeet Banerjee <abhijeetb@Abhijeets-Mac.lan>
2024-02-17 18:11:02 -0500

    Added url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://book-stack-omega.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
